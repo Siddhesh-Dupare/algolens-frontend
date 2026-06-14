57965a7fa1d2623b783dad3a61d7dbd1c4cdc800
Siddhesh Dupare <siddheshdupare92@gmail.com>
2026-06-14 10:16:18 +0530

    Init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p class="text-2xl">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <Button>Click</Button>
+</template>
+
+<style scoped></style>
